63e140afe2074a22cb7884e802d31a291a13604c
Morten Refshøj <202309636@post.au.dk>
2025-09-30 14:23:10 +0200

    Opsætning klar nu

---
+++ MobileChargingStationApp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MobileChargingStationApp.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
